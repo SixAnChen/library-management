3d4e96aabaaa9ce559f154e1d9af0f5022e085f9
Chen <sixanchen1314@gmail.com>
2023-07-26 08:59:22 +0800

    图书管理系统 项目初始化

---
+++ src/main/java/com/example/LibraryManangementApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManangementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManangementApplication.class, args);
+    }
+
+}
